54550e1ba17f7ec3c63d9c592bed759a3d0b93f7
Trung Hậu <crud5204@gmail.com>
2023-09-02 22:41:50 +0700

    fix pom

--- src/main/java/com/example/weblor/user/controller/UserController.java
+++ src/main/java/com/example/weblor/user/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.example.weblor.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class UserController {
+	
+	
+	@GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Xin chào từ Thymeleaf!");
+        return "main.html" ; // Tên của trang Thymeleaf template (ví dụ: hello.html).
+    }
 
 }
